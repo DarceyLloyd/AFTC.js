eb472b84bd25aaaa4e1947de4d199ee9fe55ef66
Darcey <Darcey.Lloyd@gmail.com>
2017-11-02 05:39:12 +0000

    updated webpacks build

--- aftc.js
+++ aftc.js
@@ -1,28 +1,29 @@
-// Requires jQuery (was initially built on v1.12, not seen any issues with 3.x yet)
-var $ = require("jquery"); // See webpack.config.js for "resolve"" path for jquery)
+// Uncomment line below to add jquery to the build
+//var $ = require("jquery"); // See webpack.config.js for "resolve"" path for jquery)
 
 // Comment out the ones you don't want/need
-import "./src/debug.js";
-import "./src/animation.js";
-import "./src/conversion.js"; 
-import "./src/cookies.js"; 
-import "./src/cordova.js"; 
-import "./src/datetime.js";  
-import "./src/detection.js"; 
-import "./src/dom.js"; 
-import "./src/form.js"; 
-import "./src/graphics.js"; 
-import "./src/io.js"; 
-import "./src/misc.js"; 
-import "./src/string.js"; 
-import "./src/validation.js"; 
+import "./src/debug.js"; // Dependencies: none
+import "./src/array.js"; // Dependencies: none
+import "./src/conversion.js"; // Dependencies: none
+import "./src/string.js"; // Dependencies: none
+import "./src/misc.js"; // Dependencies: none
+import "./src/datetime.js"; // Dependencies: none
+import "./src/detection.js"; // Dependencies: none
+import "./src/dom.js"; // Dependencies: none
+import "./src/cookies.js"; // Dependencies: none
+import "./src/form.js"; // Dependencies: none
+import "./src/graphics.js"; // Dependencies: none
+//import "./src/io.js"; // Dependencies: jQuery >= 1.12
+import "./src/validation.js"; // Dependencies: none
+import "./src/aftc.js"; // Dependencies: none
+import "./src/color.js"; // Dependencies: none
+import "./src/animation.gsap.js"; // Requires: TweenLite.min.js or TweenMax.min.js & ScrollToPlugin.min.js
+import "./src/animation.jquery.js"; // Requires: jQuery >= 1.12
+
+
+
+
 
 // Example on including lodash, axios etc from nodemodules via nodejs apps
 //var _ = require('lodash'); // https://lodash.com/
 //import "../node_modules/axios/dist/axios.min.js"; // https://github.com/mzabriskie/axios
-
-
-
-
-// This section is for more advanced features and access to the AFTC function object
-import "./src/aftc.js"; // Required
