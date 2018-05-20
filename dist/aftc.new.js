b19d86ef50b73a97c4e331fd93f234e2c6afa367
Darcey <Darcey.Lloyd@gmail.com>
2018-05-20 23:05:07 +0100

    AFTC.Point()

--- dist/aftc.new.js
+++ dist/aftc.new.js
@@ -2044,10 +2044,78 @@ window.parseJSONToSelect = function (j, elementOrId, labelKey, valueKey) {
 		element.add(option);
 	}
 }
+// AFTC.Point = function (x, y) {
+//     !x ? this.x = 0 : this.x = x;
+//     !y ? this.y = 0 : this.y = y;
+// }
+
 AFTC.Point = function (x, y) {
+
     !x ? this.x = 0 : this.x = x;
     !y ? this.y = 0 : this.y = y;
+
+    this.position = function () {
+        return [this.x, this.y];
+    }
+
+    this.clone = function () {
+        return new AFTC.Point(this.x, this.y);
+    }
+
+    this.delta = function (point) {
+        return [this.x - point.x, this.y - point.y];
+    }
+
+    this.distance = function (point) {
+        var dx = point.x - this.x;
+        var dy = point.y - this.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    this.moveTo = function(x, y) {
+        this.x = x;
+        this.y = y;
+        return this;
+    }
+
+    this.moveAtAngle = function(angle, distance) {
+        this.x += Math.cos(angle) * distance;
+        this.y += Math.sin(angle) * distance;
+        return this;
+    }
+
+    this.applyVelocity = function(velocity) {
+        this.x += velocity.vx;
+        this.y += velocity.vy;
+        return this;
+    }
+
+    this.angleRadians = function(point) {
+        // radians = atan2(deltaY, deltaX)
+        var y = point.y - this.y;
+        var x = point.x - this.x;
+        return Math.atan2(y, x);
+    }
+
+    this.angleDeg = function(point) {
+        // degrees = atan2(deltaY, deltaX) * (180 / PI)
+        var y = point.y - this.y;
+        var x = point.x - this.x;
+        return Math.atan2(y, x) * (180 / Math.PI);
+    }
+
+    this.rotate = function(origin, radians) {
+        // rotate the point around a given origin point
+        var cos = Math.cos(radians);
+        var sin = Math.sin(radians);
+        this.x =
+            cos * (this.x - origin.x) + sin * (this.y - origin.y) + origin.x;
+        this.y =
+            cos * (this.y - origin.y) - sin * (this.x - origin.x) + origin.y;
+        return this;
+    }
 }
+
 
 AFTC.Rectangle = function (x1, y1, x2, y2) {
     !x1 ? this.x1 = 0 : this.x1 = x1;
