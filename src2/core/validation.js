5dc483ca2a9da351e1b88ea7153a702d417538fe
Darcey <Darcey.Lloyd@gmail.com>
2018-05-25 04:07:28 +0100

    isEmail

--- src2/core/validation.js
+++ src2/core/validation.js
@@ -4,10 +4,9 @@
  * @param string email: email address
  * @returns boolean
  */
-window.validateEmail = function (email) {
+window.isEmail = function (email) {
 	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return re.test(email);
 }
-window.isValidEmail = function (email) {
-	return validateEmail(email);
-}
+window.validateEmail = function (email) { return isEmail(email); }
+window.isValidEmail = function (email) { return isEmail(email); }
